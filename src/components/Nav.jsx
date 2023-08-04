6703fc3afc00987dc7d6fcc474983861215404db
Christopher Steven <christopherstevennunoo@gmail.com>
2023-08-03 19:16:20 -0700

    top navbar done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,8 +5,28 @@ export default function Nav() {
   return (
     <div className='container'>
         <div className="top">
-          <img src="/logo.png" alt="" />
-          <h1>hello</h1>
+          <div className="logo">
+            <img src="/logo.png" alt="" />
+          </div>
+          <div className="toggler">
+            <img src="/sun.svg" alt="sun"/>
+            <div className="toggle-button">
+              <div className='active'></div>
+              <div className='not-active'></div>
+            </div>
+            <img src="/moon.svg" alt="moon" />
+          </div>
+        </div>
+        <div className="searchbar">
+          <div className="searchCompany">
+            <input type="text" id='companyName'/>
+          </div>
+          <div className="searchLocation">
+            <input type="text" id='companyName'/>
+          </div>
+          <div className="searchFulltime">
+            <input type="checkbox" name="fullTime" id="fullTime" />
+          </div>
         </div>
     </div>
   )
