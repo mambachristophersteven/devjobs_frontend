c971c8a720656262f0cad1fd9adaa4e8de583951
Christopher Steven <christopherstevennunoo@gmail.com>
2023-08-06 20:21:02 -0700

    Home Body and Job components

---
+++ src/components/Body.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Job from './Job'
+
+export default function Body() {
+  return (
+    <div>
+        <Job/>
+    </div>
+  )
+}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../styles/Home.css'
 import Nav from './Nav'
-
+import Body from './Body'
 export default function Home() {
   return (
     <div className="container">
         <Nav/>
+        <Body/>
     </div>
   )
 }
