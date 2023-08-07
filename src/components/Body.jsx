398f60aff6fad2b9c90e419cd06e7b6985aa9040
Christopher Steven <christopherstevennunoo@gmail.com>
2023-08-06 21:26:30 -0700

    styling body component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Job from './Job'
 import styles from '../styles/Body.module.css'
+import Button from './Button'
 
 export default function Body() {
   return (
     <div className={styles.container}>
-        <Job/>
+        <div className={styles.jobs}>
+            <Job/>
+        </div>
+        <div className={styles.load}>
+            <Button text={'Load More'}/>
+        </div>
     </div>
   )
 }
