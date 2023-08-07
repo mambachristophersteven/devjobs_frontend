1d9f322070bc306706ebaccb282ea1d5a07549e2
Christopher Steven <christopherstevennunoo@gmail.com>
2023-08-06 20:54:17 -0700

    fixing module css files

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import styles from '../styles/Job.module.css'
 
 export default function Job() {
   return (
-    <div className='company-box'>
-      <img src="/companies/scoot.png" alt="scoot company logo" className='logo'/>
-      <div className="details">
-        <div className="top-details">
-          <p className='time-and-role'>5h ago <span>. </span>Full Time</p>
-          <p className="job-title">Senior Software Engineer</p>
-          <p className="company-name">Scoot</p>
+    <div className={styles.container}>
+      <img src="/companies/scoot.png" alt="scoot company logo" className={styles.companyImage}/>
+      <div className={styles.details}>
+        <div className={styles.topDetails}>
+          <p className={styles.timeAndRole}>5h ago <span>. </span>Full Time</p>
+          <p className={styles.jobTitle}>Senior Software Engineer</p>
+          <p className={styles.companyName}>Scoot</p>
         </div>
-        <div className="bottom-details">
-          <p className="location">United Kingdom</p>
+        <div className={styles.bottomDetails}>
+          <p className={styles.location}>United Kingdom</p>
         </div>
       </div>
     </div>
