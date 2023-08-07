20684030f413673a5810c79ce78d16910919103c
Christopher Steven <christopherstevennunoo@gmail.com>
2023-08-06 20:46:20 -0700

    including module css

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Job from './Job'
+import styles from '../styles/Body.module.css'
 
 export default function Body() {
   return (
-    <div>
+    <div className={styles.container}>
         <Job/>
     </div>
   )
